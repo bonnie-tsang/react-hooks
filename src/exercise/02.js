4ada161a42c5053b59c8f6006b0bf4c63ce7c7c3
Bonnie <email.bonnietsang@gmail.com>
2021-01-05 15:01:35 +1000

    ex 2 - useEffect

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,23 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+const useLocalStorageState = (key, defaultValue = '') => {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
+  )
+  
+  React.useEffect(() => {
+    console.log('Calling useEffect');
+    window.localStorage.setItem(key, state);
+  }, [key, state])
+
+  return [state, setState];
+}
+
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName);
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +28,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +36,14 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <>
+      <button onClick={() => {setCount(count + 1)}}>{count}</button>
+      <Greeting />
+    </>
+  )
 }
 
 export default App
