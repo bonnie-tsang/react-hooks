973c2bb6f386e7c00b969105db4aa7f5fae0fd50
Bonnie <email.bonnietsang@gmail.com>
2021-01-10 14:27:58 +1000

    ex 6 extra and add demo code on ex 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -5,8 +5,22 @@ import * as React from 'react'
 // eslint-disable-next-line no-unused-vars
 import VanillaTilt from 'vanilla-tilt'
 
+
 function Tilt({children}) {
   const tiltRef = React.useRef(null);
+
+
+  // React.useEffect(()=> {
+  //   const handler = () => {
+  //     console.log("clicked");
+  //   }
+  //   document.addEventListener('click', handler);
+  //   // componentWillUnmount
+  //   return function cleanup() {
+  //     document.removeEventListener('click', handler);
+  //   }
+  // }, []);
+
 
   React.useEffect(()=> {
     const tiltNode = tiltRef.current;
@@ -21,7 +35,8 @@ function Tilt({children}) {
     return function cleanup() {
       tiltNode.vanillaTilt.destroy()
     }
-  }, [])
+  }, []);
+
 
   return (
     <div className="tilt-root" ref={tiltRef}>
@@ -31,11 +46,17 @@ function Tilt({children}) {
 }
 
 function App() {
+  const [show, setShow] = React.useState(true);
   return (
     <>
+    <div>
+      <button onClick={e => setShow(!show)}>Toggle</button>
+    </div>
+    {show && 
       <Tilt>
         <div className="totally-centered">vanilla-tilt.js</div>
       </Tilt>
+    }
     </>
   )
 }
